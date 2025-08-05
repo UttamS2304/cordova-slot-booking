f7657b7d5c3abfd2ed7e169e3e30f4b44e8dd7ec
UttamS2304 <uttamsaxena2024@gmail.com>
2025-08-05 13:03:56 +0530

    init_db.py

--- init_db.py
+++ init_db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 
 # Read SQL schema file
@@ -19,25 +18,4 @@ conn.commit()
 conn.close()
 
 print("✅ Database initialized successfully!")
-=======
-import sqlite3
 
-# Read SQL schema file
-with open("schema.sql", "r") as f:
-    schema = f.read()
-
-# Connect to (or create) SQLite database file
-conn = sqlite3.connect("cordova_publication.db")
-
-# Create a cursor to execute commands
-cursor = conn.cursor()
-
-# Run schema to create tables
-cursor.executescript(schema)
-
-# Save and close
-conn.commit()
-conn.close()
-
-print("✅ Database initialized successfully!")
->>>>>>> e8f1dd0c8cde3c0d31c3ac3867a4decb1e56d7f7
